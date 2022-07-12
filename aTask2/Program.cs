67b7d51d6a79eeb43f2d197eca214865df66b1cf
Fray <kozar20000@mail.ru>
2022-07-12 19:02:29 +0300

    task2

---
+++ aTask2/Program.cs
@@ -0,0 +1,29 @@
+﻿void printArray (int[,] matr)
+{
+    
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void fillArray (int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+int[,] matrix = new int[3, 4];
+printArray(matrix);
+fillArray(matrix);
+Console.WriteLine();
+printArray(matrix);
+
